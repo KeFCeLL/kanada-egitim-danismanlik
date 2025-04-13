d5855406c175bc5eb1a8f41613ab6c20d50f7cc4
Ozan <ozanozdeniz@gmail.com>
2025-04-13 08:40:43 -0400

    Update phone number in footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,6 @@
+          <div className="flex items-center space-x-2">
+            <PhoneIcon className="h-5 w-5 text-gray-400" />
+            <a href="tel:+905059866107" className="text-gray-300 hover:text-white">
+              +90 505 986 61 07
+            </a>
+          </div> 
